b6403505379e30ac58752c5d350b0114e27e8e7b
AKD27 <dhiman27ayush@gmail.com>
2025-07-30 04:14:02 +0530

    Optimise performance

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@
 
 import { navItems } from "@/data";
 
+import { Suspense } from "react";
+
 import Hero from "@/components/Hero";
 import Grid from "@/components/Grid";
 import Footer from "@/components/Footer";
@@ -15,14 +17,16 @@ const Home = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <FloatingNav navItems={navItems} />
-        <Hero />
-        <Grid />
-        <RecentProjects />
-        <Clients />
-        <Experience />
-        <Approach />
-        <Footer />
+        <Suspense fallback={<>Loading...</>}>
+          <FloatingNav navItems={navItems} />
+          <Hero />
+          <Grid />
+          <RecentProjects />
+          <Clients />
+          <Experience />
+          <Approach />
+          <Footer />
+        </Suspense>
       </div>
     </main>
   );
